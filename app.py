1e911ff785a41dfe054e447591f42da62a66f684
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2017-10-25 18:26:33 +0200

    creating DateConverter to retrieve date from an url (working)

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 from flask import request
+from util import DateConverter
 
 app = Flask(__name__)
+
+
+app.url_map.converters['date'] = DateConverter
 
 route = [
     {
@@ -30,10 +34,18 @@ def get_planner():
     return jsonify({'route': route,"details":details})
 
 
-@app.route('/login', methods=['GET'])
-def login():
+@app.route('/ville', methods=['GET'])
+def ville():
     ville = request.args.getlist('ville')
     return jsonify({'ville': ville})
+
+
+@app.route('/date/<date:dates>', methods=['GET'])
+def date(dates):
+	date = dates
+	date = str(date)
+	return jsonify({'date': date})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
